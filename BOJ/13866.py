ab84224f2e5e3ef353f1a6c91bd56c7b9f400005
songji <youthful_15@naver.com>
2022-04-20 23:41:05 +0900

    220420

---
+++ BOJ/13866.py
@@ -0,0 +1,3 @@
+a, b, c, d = map(int, input().split())
+
+print(min(abs(a + b - c - d), abs(a + c - b - d), abs(a + d - b - c)))
---
+++ BOJ/6064-n.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    
+    m, n, x, y = map(int, input().split())
+    
+    for i in range(min(m, n))
